a11aae8dc54e92d3576faaf50a5aa1a8233b0f61
robin <robin@shroomlife.de>
2019-09-11 16:06:32 +0200

    added new view to history

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -4,6 +4,7 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import humanizeDuration from 'humanize-duration';
+import moment from 'moment';
 
 export default function History(props) {
 	const task = props.task;
@@ -29,10 +30,21 @@ export default function History(props) {
 				{task.history.map((historyItem, itemKey) => {
 					return (
 						<Grid item xs={12} key={itemKey}>
-							<Paper className="taskItem">
-								<Typography component="p">Started: {historyItem.started}</Typography>
-								<Typography component="p">Ended: {historyItem.ended}</Typography>
-								<Typography component="p">Duration: {humanizeDuration(historyItem.duration)}</Typography>
+							<Paper className="taskItem historyItem">
+                
+		<Grid container>
+      <Grid item xs={6}>
+        <Typography component="strong">Start</Typography>
+        <Typography component="p">{moment(historyItem.started).calendar()}</Typography>
+      </Grid>
+      <Grid item xs={6}>
+        <Typography component="strong">Ended</Typography>
+        <Typography component="p">{moment(historyItem.ended).calendar()}</Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Typography component="p" className="duration">Duration: {humanizeDuration(historyItem.duration)}</Typography>
+      </Grid>
+    </Grid>
 							</Paper>
 						</Grid>
 					);
@@ -41,3 +53,5 @@ export default function History(props) {
 		</Grid>
 	);
 }
+
+const defaultMomentDateFormat = "";
